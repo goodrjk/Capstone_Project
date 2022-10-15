9fce508d3c605d79801ece4a78f8702f583d1c30
John Goodridge <menion2006@gmail.com>
2022-10-14 21:34:46 -0500

    docker image 6

--- build_upload_docker.sh
+++ build_upload_docker.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 # Step 1:
+echo "Start docker service"
+sudo docker version
+service docker.io status
+service docker.io start
+
+# Step 1:
 # Build image and add a descriptive tag
 # The . is the path
+echo "Build docker image"
 docker build --tag=capstone .
 
 # Step 2: 
@@ -15,7 +22,7 @@ docker image ls
 
 # Step 4:
 # Create dockerpath
-dockerpath=goodrjk/udacity_5:firstimage
+dockerpath=goodrjk/udacity_5:latest
 
 # Step 5:  
 # Authenticate & tag
@@ -24,6 +31,7 @@ docker tag capstone $dockerpath
 
 # Step 6:
 # Push image to a docker repository
+echo "Push image to DockerHub"
 docker login
 docker push $dockerpath
 #docker push goodrjk/udacity_4:tagname
